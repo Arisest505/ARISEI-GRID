23c2f1a1529d8831ff32b4942c72810b8331b149
Arisest505 <arisescrp@gmail.com>
2025-08-09 21:13:52 -0500

    Mejoras de la Beta 1.0.3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,24 +3,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/router";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext"; //  Aquí
+import { AuthProvider } from "./context/AuthContext";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Toaster } from "sonner";
-import ScrollToTop from "./components/ScrollToTop"; // Importa ScrollToTop
-
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-     <DndProvider backend={HTML5Backend} >
-          <BrowserRouter>
-            <ScrollToTop /> {/* Agrega ScrollToTop aquí */}
-                <Toaster position="top-center" richColors expand />
-                  <AuthProvider> 
-                    <App />
-                  </AuthProvider>
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <AuthProvider>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Toaster position="top-center" richColors expand />
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </DndProvider>
-
   </React.StrictMode>
 );
