1fd5da76be98609719267c4fdd6cf0b2ca27286f
Arisest505 <arisescrp@gmail.com>
2025-08-10 20:50:29 -0500

    Mejoras de la Beta 1.1.7

--- frontend/src/widgets/NavBar.tsx
+++ frontend/src/widgets/NavBar.tsx
@@ -317,7 +317,7 @@ export default function NavBar() {
               </button>
             ) : (
               <button
-                onClick={() => navigate("/")}
+                onClick={() => navigate("/auth")}
                 className="flex items-center justify-center flex-1 gap-2 px-3 py-2 text-sm font-semibold text-white transition rounded-md bg-cyan-500 hover:bg-cyan-600 focus:outline-none focus:ring-2 focus:ring-cyan-500/40"
                 style={{ transitionDuration: "var(--trs)" }}
               >
